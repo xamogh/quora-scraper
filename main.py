10aa65af7c1ed6e1531e092cc5be104328910ad2
Amogh Rijal <amoghrijal@Amoghs-MacBook-Air.local>
2022-11-15 18:57:47 +0545

    Initial setup

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup;
+from selenium import webdriver;
+from selenium.webdriver.chrome.options import Options
+
+class Scraper:
+    def __init__(self):
+        chrome_options = Options();
+        chrome_options.add_argument("--headless");
+        chrome_options.add_argument("--disable-extensions");
+        chrome_options.add_argument("--disable-gpu");
+
+        self.driver = webdriver.Chrome(options=chrome_options);
+
+class QuoraProfileScraper(Scraper):
+    def __init__(self, url):
+        self.url = url;
+        Scraper.__init__(self);
+
+    def scrape(self): 
+       self.driver.get(self.url); 
+       print(self.driver.page_source.encode("utf-8"));
+
+amoghQuoraScraper = QuoraProfileScraper("https://www.quora.com/profile/Amogh-Rijal");
+amoghQuoraScraper.scrape();
