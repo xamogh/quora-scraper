96dba2878c9737c2e2df4ed7ea2abb5d03b47fd6
Amogh Rijal <amoghrijal@Amoghs-MacBook-Air.local>
2022-11-15 21:37:56 +0545

    finds answer button

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
-from bs4 import BeautifulSoup;
-from selenium import webdriver;
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 class Scraper:
     def __init__(self):
-        chrome_options = Options();
-        chrome_options.add_argument("--headless");
-        chrome_options.add_argument("--disable-extensions");
-        chrome_options.add_argument("--disable-gpu");
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-gpu")
 
-        self.driver = webdriver.Chrome(options=chrome_options);
+        self.driver = webdriver.Chrome(options=chrome_options)
+
 
 class QuoraProfileScraper(Scraper):
     def __init__(self, url):
-        self.url = url;
-        Scraper.__init__(self);
+        self.url = url
+        Scraper.__init__(self)
 
-    def scrape(self): 
-       self.driver.get(self.url); 
-       print(self.driver.page_source.encode("utf-8"));
+    def scrape(self):
+        self.driver.get(self.url)
+        WebDriverWait(self.driver, 120).until(
+            EC.presence_of_element_located((By.ID, "root"))
+        )
+        answerButton = self.__get_answer_button()
+        if answerButton:
+            answerButton.click()
+
+    def __get_answer_button(self):
+        answer_buttons = self.driver.find_elements(
+            By.CLASS_NAME,
+            "iyYUZT.ClickWrapper___StyledClickWrapperBox-zoqi4f-0.qu-cursor--pointer.qu-tapHighlight--white",
+        )
+        for button in answer_buttons:
+            if button.text.__contains__("Answers"):
+                innerContentArr = button.text.split()
+                print(innerContentArr, innerContentArr[1])
+                if innerContentArr[1] == "Answers":
+                    return button
+
 
 if __name__ == "__main__":
-    amoghQuoraScraper = QuoraProfileScraper("https://www.quora.com/profile/Amogh-Rijal");
-    amoghQuoraScraper.scrape();
-
+    amoghQuoraScraper = QuoraProfileScraper("https://www.quora.com/profile/Amogh-Rijal")
+    amoghQuoraScraper.scrape()
